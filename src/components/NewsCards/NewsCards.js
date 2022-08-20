b6dfb240947198461a2a8f554aae5722d751e35f
VuDej <drvu47@gmail.com>
2022-08-20 15:09:50 +0200

    all works

---
+++ src/components/NewsCards/NewsCards.js
@@ -0,0 +1,44 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react';
+import { Grid, Grow, Typography } from '@material-ui/core';
+
+import NewsCard from './NewsCard/NewsCard';
+import useStyles from './styles.js';
+
+const infoCard = { color: '#00838f', title: 'This is advanced React app with voice navigation', text: 'It will open instructions how to use application. After that you will read instructions, when you are ready just say "Close instructions", and you can be available to search latest world news, news from specific news agency, news for specific country and category.' };
+
+const NewsCards = ({ articles, activeArticle }) => {
+  const classes = useStyles();
+
+  if (!articles.length) {
+    return (
+      <Grow in>
+        <Grid className={classes.container}>
+          <infoCard>
+            <Grid item xs={12} className={classes.infoCard}>
+              <div className={classes.card} style={{ backgroundColor: infoCard.color }}>
+                <Typography variant="h5" component="h5">{infoCard.title}</Typography>
+                {infoCard.info ? <Typography variant="h6" component="h6"><strong>{infoCard.title.split(' ')[2]}</strong>: <br />{infoCard.info}</Typography> : null}
+                <Typography variant="h6" component="h6">For start, say 'Open instructios' : <br /> <i>{infoCard.text}</i></Typography>
+              </div>
+            </Grid>
+          </infoCard>
+        </Grid>
+      </Grow>
+    );
+  }
+
+  return (
+    <Grow in>
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {articles.map((article, i) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}>
+            <NewsCard activeArticle={activeArticle} i={i} article={article} />
+          </Grid>
+        ))}
+      </Grid>
+    </Grow>
+  );
+};
+
+export default NewsCards;
