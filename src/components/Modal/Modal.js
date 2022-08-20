becbba7264167731d848981b27c6348234de0e24
VuDej <drvu47@gmail.com>
2022-08-20 15:20:51 +0200

    all works

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 import React from 'react';
-import { Typography, Divider, Chip, TextField, Button } from '@material-ui/core';
+import { Typography, Chip, TextField, Button } from '@material-ui/core';
 import SimpleModal from '@material-ui/core/Modal';
 import emailjs from 'emailjs-com';
 
